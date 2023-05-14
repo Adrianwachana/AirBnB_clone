c22ca625c322d5c772b09c0ab578d110c2e31025
uptownkiddo <muchokilewis8@gmail.com>
2023-05-14 02:29:39 -0400

    console.py -correction

--- console.py
+++ console.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
-def do_quit(self, arg):
-    '''exit the program'''
-    return True
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
 
-def do_EOF(self, arg):
-    '''Exit the program'''
-    print()
-    return True
+    def do_EOF(self, arg):
+        """Exit the program"""
+        return True
 
-def emptyline(self):
-    '''Do nothing for an empty line'''
-    pass
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
 
-if __name__ == "__main__":
+    def help_quit(self):
+        """Display help message for quit command"""
+        print("Exit the program.")
+
+    def help_EOF(self):
+        """Display help message for EOF command"""
+        print("Exit the program.")
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
