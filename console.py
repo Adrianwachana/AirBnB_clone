67d7358b8d335c97014aec8a867295db394b5683
uptownkiddo <muchokilewis8@gmail.com>
2023-05-14 09:11:36 -0400

    console.py

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
-'''this module has the HBNBCommand class'''
-
+#!/usr/bin/env python3
+"""This module hosts the HBNBCommand class which inherits from the Cmd class.
+"""
 import cmd
 from datetime import datetime
 from models import storage
@@ -12,33 +12,51 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-class HBNBCommand(cmd.Cmd):
-    '''defines the prompt of the console'''
 
-    prompt = "(hbnb) "
+class HBNBCommand(cmd.Cmd):
+    """
+    Entry point Class HBNBCommand
+    defines a prompt (hbnb)
+
+    public class methods:
+        do_quit - handles quit command
+        do_EOF - handles exit using EOF
+    """
+
+    prompt = '(hbnb) '
+    __models = ["BaseModel", "User", "State",
+                "City", "Amenity", "Place", "Review"]
+
+    def emptyline(self):
+        """
+        Override the default emptyline method
+        to avoid repeating the last command
+        when an empty line is entered.
+        """
+        pass
 
     def do_quit(self, arg):
-        """Command to exit the program"""
+        """type quit to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """Exit the program"""
-        print()
+        """Ctrl + D exits the program"""
+        print()  # Print a new line before exiting
         return True
 
-    def emptyline(self):
-        """Do nothing when an empty line is entered"""
-        pass
-
     def do_create(self, arg):
-        '''creates a new instance of BaseModel, saves it and prints the id'''
+        """create command creates a new instance of a specified class
+
+        example usage:
+        (hbnb) create BaseModel
+        """
         if not arg:
-            print("**class name missing**")
+            print('** class name missing ** ')
             return
 
         class_name = arg.strip()
         if class_name not in self.__models:
-            print("**class doesn't exist**")
+            print("** class doesn't exist **")
             return
 
         new_instance = globals()[class_name]()
@@ -46,7 +64,12 @@ class HBNBCommand(cmd.Cmd):
         print(new_instance.id)
 
     def do_show(self, arg):
-        """Prints the string representation of an instance based on the class name and id"""
+        """Prints the string representation of an instance based on the class
+        name and id.
+
+        example usage:
+        (hbnb) show BaseModel 1234-5665-4321
+        """
 
         try:
             [class_name, instance_id] = self.get_args(arg)
@@ -62,7 +85,11 @@ class HBNBCommand(cmd.Cmd):
             pass
 
     def do_destroy(self, arg):
-        """Destroys an instance of a class based on the class name and id."""
+        """Destroys an instance of a class based on the class name and id.
+
+        Example usage:
+        (hbnb) destroy BaseModel 1234-5665-4321
+        """
 
         try:
             [class_name, instance_id] = self.get_args(arg)
@@ -80,7 +107,12 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, arg):
         """
         Prints all string representation of all instances
-        based or not on the class name."""
+        based or not on the class name.
+
+        example usage:
+        (hbnb) all
+        (hbnb) all BaseModel
+        """
 
         if not arg:
             # Print all instances
@@ -104,7 +136,10 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id by adding or
-        updating attribute (save the change into the JSON file)"""
+        updating attribute (save the change into the JSON file)
+
+        example usage:
+        (hbnb) update BaseModel 1234-5676-4321 email "aibnb@mail.com" """
 
         args = self.get_update_args(arg)
 
@@ -127,22 +162,70 @@ class HBNBCommand(cmd.Cmd):
         except Exception:
             pass
 
-    def do_show(self, arg):
-        """Prints the string representation of an instance based on the class
-        name and id."""
+    def get_update_args(self, arg):
+        args = arg.split()
 
+        if args:
+            if len(args) > 1:
+                if len(args) > 2:
+                    if len(args) > 3:
+                        return [*args[:3] + [self.get_value(args)]]
+                    else:
+                        print("** value missing **")
+                else:
+                    print("** attribute name missing **")
+            else:
+                print("** instance id missing **")
+        else:
+            print('** class name missing **')
+
+    def get_value(self, args):
+        found = False
+        arr = " ".join([str(item) for item in args[3:]])
+        value = ""
+
+        for j in arr:
+            if found and j == '"':
+                found = not found
+                break
+            elif not found and j == '"':
+                found = not found
+            else:
+                value += j
+
+        return value
+
+    def get_args(self, arg):
+        args = arg.split()
+
+        # check if the array has content
+        if args:
+            # check if 2 arguments were passed
+            if len(args) == 2:
+                return args
+            else:
+                print("** instance id missing **")
+        else:
+            print('** class name missing **')
+        return None
+
+    def find_record(self, class_name, instance_id):
+        # check if valid class_name
+        if class_name not in self.__models:
+            print("** class doesn't exist **")
+            return
+
+        # retrieve all records in storage
+        all = storage.all()
+
+        # check for matching record
         try:
-            [class_name, instance_id] = self.get_args(arg)
-
-            if class_name not in self.__models:
-                print("** class doesn't exist **")
-                return
-
-            record = self.find_record(class_name, instance_id)
-            retrieved_record = globals()[class_name](**record)
-            print(retrieved_record)
+            record = all[class_name + "." + instance_id]
+            return record
         except Exception:
-            pass
+            print("** no instance found **")
+            return None
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
